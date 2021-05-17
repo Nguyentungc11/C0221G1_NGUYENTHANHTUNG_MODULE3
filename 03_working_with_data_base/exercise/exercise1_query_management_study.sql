52c6fa23222df1cf9ec5fa61893189701c1f2fbc
Nguyentungnc11 <thanhtungnguyenddk@gmail.com>
2021-05-17 08:24:35 +0700

    fix

--- 03_working_with_data_base/exercise/exercise1_query_management_study.sql
+++ 03_working_with_data_base/exercise/exercise1_query_management_study.sql
@@ -5,7 +5,7 @@ where student_name like "H%";
 
 select *
 from class
-where start_date like "%09%";
+where start_date like "%12%";
 
 select *
 from subjects
@@ -15,9 +15,10 @@ update student
 set class_id = 2 
 where student_name = "Hùng";
 
+
 select s.student_name,sub.sub_name,m.mark 
 from student s join mark m
 on s.student_id = m.student_id
 join subjects sub
 on sub.sub_id = m.sub_id
-order by mark DESC;
+order by mark DESC,student_name;
